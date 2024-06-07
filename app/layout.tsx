5837495a5f65c865e3466ba5a97507c8a1805d5c
ethan <ethan@duomai.com>
2024-06-07 18:53:53 +0800

    添加代码测试

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,27 @@ export default function RootLayout({ children }: PageProps) {
 
   gtag('config', 'G-VD3D5Q376Y');`}
         </Script>
+        {/* <!-- Google Tag Manager --> */}
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PMMKPWX');`}</script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5PMMKPWX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+        {children}
+      </body>
     </html>
   );
 }
