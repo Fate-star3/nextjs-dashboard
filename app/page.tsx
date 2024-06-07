cbde879e76d8498cc1392d26d19f3fab146ebbec
ethan <ethan@duomai.com>
2024-06-07 18:18:19 +0800

    重新push

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { ClickLog } from '@/app/components/log/ClickLog';
 export default function Page() {
   return (
     <RootPage>
+        
       <ViewLog module="Home" target="dashboard">
         <main className="flex min-h-screen flex-col p-6">
           <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
