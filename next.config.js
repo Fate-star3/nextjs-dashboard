f35ad57f925f29074877f279619e4a4e11e4ca7e
ethan <ethan@duomai.com>
2024-06-07 18:42:11 +0800

    添加自定义的next-env文件

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compilerOptions: {
+    skipLibCheck: true,
+    target: 'es5',
+    lib: ['dom', 'dom.iterable', 'esnext'],
+    allowJs: true,
+    skipLibCheck: true,
+    strict: false,
+    forceConsistentCasingInFileNames: true,
+    noEmit: true,
+    esModuleInterop: true,
+    module: 'esnext',
+    moduleResolution: 'node',
+    resolveJsonModule: true,
+    isolatedModules: true,
+    jsx: 'preserve',
+    incremental: true,
+    types: ['new-types'],
+  },
+  include: [
+    'new-types.d.ts',
+    'next-env.d.ts',
+    '.next/types/**/*.ts',
+    '**/*.ts',
+    '**/*.tsx',
+  ],
+  exclude: ['node_modules'],
+};
 
 module.exports = nextConfig;
