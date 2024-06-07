c9d5852841b09387c3bb0283dbba2010ae15a2f9
ethan <ethan@duomai.com>
2024-06-07 14:40:50 +0800

    添加谷歌分析代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import NextHead from 'next/head';
+import Script from 'next/script';
+import { HTMLAttributes, PropsWithChildren } from 'react';
+type PageProps = PropsWithChildren<any> & HTMLAttributes<HTMLDivElement>;
+export default function RootLayout({ children }: PageProps) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VD3D5Q376Y"
+        ></Script>
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VD3D5Q376Y');`}
+        </Script>
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
